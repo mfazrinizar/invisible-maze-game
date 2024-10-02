2d3a47cb8c00a4eed5e75c494788f838118a77ee
M. Fazri Nizar <anon6372098.id@gmail.com>
2024-10-02 23:47:42 +0700

    Feat: add maze codes

---
+++ src/maze/RecursiveBacktracker.java
@@ -0,0 +1,149 @@
+package maze;
+
+import java.util.Random;
+
+import coords.Node;
+import coords.Point;
+
+/**
+ * Implements the recursive backtracking algorithm. The algorithm works by
+ * exploring unvisited nodes using randomized depth-first search.
+ */
+public class RecursiveBacktracker extends Maze {
+    private static final long serialVersionUID = -5689416515127359434L;
+
+    private final Random rnd;
+    private final Node start;
+
+    /**
+     * Sets the dimensions of the maze.
+     *
+     * @param  width the width of the maze
+     * @param  height the height of the maze
+     * @throws IllegalArgumentException if width or height is not positive
+     */
+    public RecursiveBacktracker(int width, int height) {
+        super(width, height);
+        rnd = new Random();
+        start = new Node(rnd.nextInt(width), rnd.nextInt(height));
+    }
+
+    /**
+     * Sets the dimensions of the maze and the starting point of the maze
+     * generation algorithm.
+     *
+     * @param  width the width of the maze
+     * @param  height the height of the maze
+     * @param  startX the x-coordinate of the algorithm's starting point
+     * @param  startY the y-coordinate of the algorithm's starting point
+     * @throws IllegalArgumentException if width or height is not positive
+     * @throws OutOfBoundsException if (x, y) is out of bounds
+     */
+    public RecursiveBacktracker(int width, int height, int startX, int startY) {
+        super(width, height);
+        checkBounds(startX, startY);
+        start = new Node(startX, startY);
+        rnd = new Random();
+    }
+
+    /**
+     * Sets the dimensions of the maze and the starting point of the maze
+     * generation algorithm.
+     *
+     * @param  width the width of the maze
+     * @param  height the height of the maze
+     * @param  start the algorithm's starting point
+     * @throws IllegalArgumentException if width or height is not positive
+     * @throws OutOfBoundsException if start is out of bounds
+     * @throws NullPointerException if start is null
+     */
+    public RecursiveBacktracker(int width, int height, Point start) {
+        this(width, height, start.getX(), start.getY());
+    }
+
+    /**
+     * Sets the starting point of the maze generation algorithm.
+     *
+     * @param  startX the x-coordinate of the algorithm's starting point
+     * @param  startY the y-coordinate of the algorithm's starting point
+     * @throws OutOfBoundsException if (x, y) is out of bounds
+     */
+    public void setStart(int x, int y) {
+        checkBounds(x, y);
+        start.set(x, y);
+    }
+
+    /**
+     * Sets the starting point of the maze generation algorithm.
+     *
+     * @param  start the algorithm's starting point
+     * @throws OutOfBoundsException if start is out of bounds
+     * @throws NullPointerException if start is null
+     */
+    public void setStart(Point start) {
+        setStart(start.getX(), start.getY());
+    }
+
+    /** Returns the starting point of the maze generation algorithm. */
+    public Point getStart() {
+        return new Node(start);
+    }
+
+    @Override
+    public void generate() {
+        fill();
+        recursiveBacktrack(start);
+    }
+
+    private void recursiveBacktrack(Node start) {
+        Direction[] directions = Direction.values();
+        Direction[] moves = new Direction[4];
+        int unvisited = getWidth() * getHeight() - 1;
+
+        Node current = new Node(start);
+
+        while (unvisited > 0) {
+            /* Finds adjacent unvisited nodes. */
+            int moveCount = getMoves(current, moves);
+
+            /* Takes a step back if there are no such nodes. */
+            if (moveCount == 0) {
+                /* Moves in the reverse of the saved direction. */
+                Direction rev = directions[getFlags(current)].getReverse();
+                current.translate(rev.dx, rev.dy);
+                continue;
+            }
+
+            /* Picks a random adjacent unvisited node and adds it to the maze. */
+            Direction d = moves[rnd.nextInt(moveCount)];
+            removeWall(current, d);
+
+            /* Updates the current node to the newly added node. */
+            current.translate(d.dx, d.dy);
+
+            /* Saves the direction taken to reach the current node. */
+            setFlags(current, d.ordinal());
+
+            --unvisited;
+        }
+    }
+
+    /** Gets the directions which point to adjacent unvisited nodes. */
+    private int getMoves(Node n, Direction[] moves) {
+        int count = 0;
+        if (n.y > 0 && isUnvisited(n.x, n.y - 1)) {
+            moves[count++] = Direction.NORTH;
+        }
+        if (n.x < getWidth() - 1 && isUnvisited(n.x + 1, n.y)) {
+            moves[count++] = Direction.EAST;
+        }
+        if (n.y < getHeight() - 1 && isUnvisited(n.x, n.y + 1)) {
+            moves[count++] = Direction.SOUTH;
+        }
+        if (n.x > 0 && isUnvisited(n.x - 1, n.y)) {
+            moves[count++] = Direction.WEST;
+        }
+        return count;
+    }
+}
+
---
+++ src/maze/TileMaze.java
@@ -0,0 +1,127 @@
+package maze;
+
+
+import java.io.Serializable;
+
+import coords.Point;
+import controller.OutOfBoundsException;
+
+/** Tile maze wrapper for {@link Maze}. */
+public class TileMaze implements Serializable {
+    private static final long serialVersionUID = -5050221133107186563L;
+
+    private Maze maze;
+    private int width;
+    private int height;
+
+    /**
+     * Creates a tile maze wrapper around the specified {@link Maze} object.
+     *
+     * @param  maze the {@link Maze} object to be wrapped
+     * @throws NullPointerException if maze is null
+     */
+    public TileMaze(Maze maze) {
+        setMaze(maze);
+    }
+
+    /**
+     * Wraps this around the specified {@link Maze} object.
+     *
+     * @param  maze the {@link Maze} object to be wrapped
+     * @throws NullPointerException if maze is null
+     */
+    public void setMaze(Maze maze) {
+        if (maze == null) {
+            throw new NullPointerException();
+        }
+        this.maze = maze;
+        width = 2 * maze.getWidth() + 1;
+        height = 2 * maze.getHeight() + 1;
+    }
+
+    /** Returns the internal {@link Maze} object. */
+    public Maze getMaze() {
+        return maze;
+    }
+
+    /** Generates the maze. */
+    public void generate() {
+        maze.generate();
+    }
+
+    /** Returns the width of the maze in tiles. */
+    public int getWidth() {
+        return width;
+    }
+
+    /** Returns the height of the maze in tiles. */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Checks if the tile at the specified coordinates is a wall.
+     *
+     * @param  x the x-coordinate of the tile
+     * @param  y the y-coordinate of the tile
+     * @return true if the tile at (x, y) is a wall
+     * @throws OutOfBoundsException if (x, y) is out of bounds
+     */
+    public boolean isWall(int x, int y) {
+        checkBounds(x, y);
+        boolean xIsEven = (x % 2 == 0);
+        boolean yIsEven = (y % 2 == 0);
+        if (xIsEven && yIsEven) {
+            return true;
+        }
+        if (x == 0) {
+            return maze.isWall(0, (y - 1) / 2, Direction.WEST);
+        }
+        if (y == 0) {
+            return maze.isWall((x - 1) / 2, 0, Direction.NORTH);
+        }
+        if (xIsEven) {
+            return maze.isWall((x - 1) / 2, (y - 1) / 2, Direction.EAST);
+        }
+        if (yIsEven) {
+            return maze.isWall((x - 1) / 2, (y - 1) / 2, Direction.SOUTH);
+        }
+        return false;
+    }
+
+    /**
+     * Checks if the tile at the specified coordinates is a wall.
+     *
+     * @param  p the coordinates of the tile to check
+     * @return true if the tile at p is a wall
+     * @throws OutOfBoundsException if p is out of bounds
+     * @throws NullPointerException if p is null
+     */
+    public boolean isWall(Point p) {
+        return isWall(p.getX(), p.getY());
+    }
+
+    private void checkBounds(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            throw new OutOfBoundsException("(" + x + ", " + y + ")");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String lineSeparator = System.getProperty("line.separator");
+        StringBuilder builder = new StringBuilder();
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                if (isWall(x, y)) {
+                    builder.append("# ");
+                } else {
+                    builder.append("  ");
+                }
+            }
+            builder.append(lineSeparator);
+        }
+        return builder.toString();
+    }
+}
+
